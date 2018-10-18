adbd93e4887a5d10573a50c8da7ce1e40f880f71
Pellstrike <pellstrike@gmail.com>
2018-10-18 15:23:43 +0900

    Add grade field to the user app

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    grade = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(4)], default=1
+    )
