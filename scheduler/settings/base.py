46827279f1e2f6f129bc98523d03f37c55f92735
pellstrike <pellstrike@gmail.com>
2018-11-29 13:36:35 +0900

    Reduce the size of contents of single pagination

--- scheduler/settings/base.py
+++ scheduler/settings/base.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 20
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
