9518d56e6308d5ccdfffd2a720f2add1a9dd234a
pellstrike <pellstrike@gmail.com>
2018-12-01 14:11:41 +0900

    Add generate_possible_lectures method that generate combinations for lectures with given filtering attributes

--- apps/lecture/views.py
+++ apps/lecture/views.py
@@ -1,8 +1,11 @@
+import itertools
+
 from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter
 from rest_framework import status
 
 from .models import Lecture, LectureTime, Category, Subcategory
@@ -18,14 +21,14 @@ def convert_time(time):
 def filter_lecture_time(queryset, start, end, day=None):
     # start query for 'start <= time < end'
     start_gte = Q(timetable__start__gte=start)
-    start_lte = Q(timetable__start__lt=end)
+    start_lt = Q(timetable__start__lt=end)
     # end query for 'start < time <= end'
-    end_gte = Q(timetable__end__gt=start)
+    end_gt = Q(timetable__end__gt=start)
     end_lte = Q(timetable__end__lte=end)
 
     if day is None:
-        return queryset.exclude((start_gte & start_lte) | (end_gte & end_lte))
-    return queryset.exclude(Q(timetable__day=day) & ((start_gte & start_lte) | (end_gte & end_lte)))
+        return queryset.exclude((start_gte & start_lt) | (end_gt & end_lte))
+    return queryset.exclude(Q(timetable__day=day) & ((start_gte & start_lt) | (end_gt & end_lte)))
 
 
 def filter_lecture(queryset, name, value):
@@ -130,11 +133,11 @@ class LectureSearchFilter(filters.FilterSet):
 
     def filter_category(self, queryset, name, value):
         category = Category.CATEGORIES.get(value, Category.NONE)
-        return queryset.filter(category__category__icontains=category)
+        return queryset.filter(category__category=category)
 
     def filter_subcategory(self, queryset, name, value):
         subcategory = Subcategory.SUBCATEGORIES.get(value, Subcategory.NONE)
-        return queryset.filter(subcategory__subcategory__icontains=subcategory)
+        return queryset.filter(subcategory__subcategory=subcategory)
 
 
 class LectureSearchAPIView(ListAPIView):
@@ -143,8 +146,9 @@ class LectureSearchAPIView(ListAPIView):
     """
     serializer_class = LectureSerializer
     queryset = Lecture.objects.prefetch_related('timetable')
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (SearchFilter, filters.DjangoFilterBackend,)
     filterset_class = LectureSearchFilter
+    search_fields = ('title', 'department__title', 'professor',)
 
     def list(self, request, *args, **kwargs):
         lectures = self.filter_queryset(self.get_queryset())
@@ -178,20 +182,60 @@ class LectureQueryAPIView(ListAPIView):
     Query a lectures with given URL parameters. Returns list of all the possible lectures.
     """
     serializer_class = LectureSerializer
-    queryset = Lecture.objects.prefetch_related('timetable').order_by('id')
+    queryset = Lecture.objects.prefetch_related('timetable')
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = LectureQueryFilter
 
+    def generate_possible_lectures(self, filterset):
+        result = []
+
+        selected = self.request.GET.get('selected').split(',')
+        selected = [x.strip() for x in selected]
+
+        fixed = self.request.GET.get('fixed').split(',')
+        fixed = [int(x) for x in fixed]
+        fixed = list(Lecture.objects.filter(pk__in=fixed))
+
+        lectures = list()
+        for code in selected:
+            lectures.append(list(filterset.filter(code=code)))
+        lectures = list(itertools.product(*lectures))
+
+        for lecture in lectures:
+            lecture_query = LectureTime.objects.filter(lecture__in=[x.id for x in lecture])
+            duplicated = False
+            for time in lecture_query:
+                start_gte = Q(start__gte=time.start)
+                start_lt = Q(start__lt=time.end)
+                end_gt = Q(end__gt=time.start)
+                end_lte = Q(end__lte=time.end)
+                timetable = lecture_query.filter(Q(day=time.day) & ((start_gte & start_lt) | (end_gt & end_lte)))
+                # lecture_query has duplicated timetable - remove it from the result list.
+                if timetable.count() >= 2:
+                    duplicated = True
+                    break
+            if not duplicated:
+                result.append(list(lecture))
+
+        # Add fixed lectures to result.
+        result = [x + fixed for x in result]
+        return result
+
     def list(self, request, *args, **kwargs):
         lectures = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(lectures)
+        # NOTE: Without this filtering method, generate_possible_lectures won't work as queries are too complex.
+        # You should never remove this unless you 100% sure about what you're doing.
+        result_list = self.generate_possible_lectures(Lecture.objects.filter(pk__in=[x.id for x in list(lectures)]))
 
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+        result = list()
+        for entry in result_list:
+            sublist = list()
+            for lecture in entry:
+                serializer = self.get_serializer(lecture)
+                sublist.append(serializer.data)
+            result.append(sublist)
 
-        serializer = self.get_serializer(lectures, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(result, status=status.HTTP_200_OK)
 
 
 class CategoryListAPIView(ListAPIView):
