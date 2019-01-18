ae6a8204b9fca7f6bbacf8598257eb4f2213c121
James <jamesj0107@naver.com>
2019-01-18 16:54:24 +0900

    Make lecture parser universal for other semesters, and fix bugs when the count of timetable and classrooms differed, data didn't parsed correctly

--- apps/lecture/parser.py
+++ apps/lecture/parser.py
@@ -1,3 +1,5 @@
+import re
+
 from openpyxl import load_workbook
 
 from .models import (
@@ -11,11 +13,23 @@ def create_parser(path, worksheet):
     return ws
 
 
-def parse_lecture_data(use_log):
-    """
-    Parse the lecture.xlsx file and create a lecture database.
-    """
-    lecture_data = create_parser('lectures.xlsx', '강의시간표(schedule)')
+def split_string(string):
+    if string is None:
+        return ['']
+
+    elif '/' in string:
+        return string.split('/')
+
+    elif ',' in string:
+        return string.split(',')
+
+    else:
+        return [string]
+
+
+def parse_lecture_data(file, use_log):
+
+    lecture_data = create_parser(file, '강의시간표(schedule)')
 
     first_row = False
     for row in lecture_data.rows:
@@ -24,31 +38,20 @@ def parse_lecture_data(use_log):
             first_row = True
             continue
 
-        title = row[4].value            # column E
-        code = row[2].value             # column C
-        division = row[3].value         # column D
-        grade = int(row[7].value)       # column H
-        point = float(row[8].value)     # column I
+        title = row[4].value  # column E
+        code = row[2].value  # column C
+        division = row[3].value  # column D
+        grade = int(row[7].value)  # column H
+        point = float(row[8].value)  # column I
+        lecture_type = Lecture.LECTURE_TYPE.get(row[5].value)  # column F
+        language = Lecture.LANGUAGE_TYPE.get(row[18].value)  # column S
 
-        # column F
-        lecture_type = Lecture.LECTURE_TYPE.get(row[5].value)
+        department, created = Department.objects.get_or_create(title=row[1].value)  # column B
+        origin_dept, created = Department.objects.get_or_create(title=row[14].value)  # column O
 
-        # column G
-        categories, subcategories = categorize_lecture(title)
+        classroom = row[12].value  # column M
+        professor = row[13].value  # column N
 
-        # column S
-        language = Lecture.LANGUAGE_TYPE.get(row[18].value)
-
-        # column B
-        department, created = Department.objects.get_or_create(title=row[1].value)
-
-        # column O
-        origin_dept, created = Department.objects.get_or_create(title=row[14].value)
-
-        classroom = row[12].value       # column M
-        professor = row[13].value       # column N
-
-        # Create a corresponding object for an input.
         lecture = Lecture.objects.create(
             title=title, code=code, division=division, grade=grade, point=point,
             type=lecture_type, language=language,
@@ -56,57 +59,55 @@ def parse_lecture_data(use_log):
             classroom=classroom, professor=professor,
         )
 
-        # Add categories and subcategories
-        for category in categories:
-            item, created = Category.objects.get_or_create(category=category)
-            lecture.category.add(item)
-
-        for subcategory in subcategories:
-            item, created = Subcategory.objects.get_or_create(subcategory=subcategory)
-            lecture.subcategory.add(item)
-
-        # column L
-        rawtimes = row[11].value
+        rawtimes = row[11].value  # column L
 
         # if value is None, skip the current row
         if rawtimes is None:
             continue
 
-        # split time with comma
-        rawtimes = rawtimes.split(',')
-        if classroom is not None:
-            places = classroom.split(',')
-            for (rawtime, place) in zip(rawtimes, places):
-                rawtime = rawtime.split()
-                place = LectureTime.PLACE_TYPE.get(place.strip()[0], None)
+        # split time and places
+        rawtimes = split_string(rawtimes)
+        places = split_string(classroom)
 
-                days_cache = []
-                for token in rawtime:
-                    # add all days before time range into days_cache
-                    if token in LectureTime.DAYS:
-                        days_cache.append(LectureTime.DAYS[token])
-                    # create time range with days
-                    elif len(token) > 2:
-                        times = token.split('~')
-                        for days in days_cache:
-                            LectureTime.objects.create(
-                                lecture=lecture, place=place, start=times[0], end=times[1], day=days)
-                        days_cache.clear()
-        else:
-            for rawtime in rawtimes:
-                rawtime = rawtime.split()
-                days_cache = []
-                for token in rawtime:
-                    # add all days before time range into days_cache
-                    if token in LectureTime.DAYS:
-                        days_cache.append(LectureTime.DAYS[token])
-                    # create time range with days
-                    elif len(token) > 2:
-                        times = token.split('~')
-                        for days in days_cache:
-                            LectureTime.objects.create(
-                                lecture=lecture, place=None, start=times[0], end=times[1], day=days)
-                        days_cache.clear()
+        # final time data stored
+        # e.g. if the given data is '월 수 16:30~18:00, 수 18:00~20:00'
+        # time_data = [['월', ['16:30', '18:00']], ['수', ['16:30', '18:00']], ['수', ['18:00', '20:00']]]
+        time_data = []
+
+        # regex for finding time in string
+        # e.g. if the given data is '월 수 16:30~18:00'
+        # regex finds 16:30, 18:00
+        time_regex = re.compile('\d\d:\d\d')
+
+        # generate time_data with given data
+        for rawtime in rawtimes:
+            rawtime.strip()
+            times = time_regex.findall(rawtime)
+            days = [day for day in rawtime[0:rawtime.find(times[0])] if day is not ' ']
+
+            for day in days:
+                time_data.append([day, times])
+
+        # e.g. time_data is 월 수 16:30~18:00 and classroom is 광715
+        # e.g. time_data is 월 수 16:30~18:00, 수 18:00~20:00 and classroom is 율301,동401
+        if len(time_data) > len(places):
+            while len(time_data) > len(places):
+                places.insert(0, places[0])
+
+        # e.g. time_data is 월 16:30~18:00 and classroom is 율301,동401
+        # Doesn't really make sense
+        elif len(time_data) < len(places):
+            places = [','.join(places)]
+
+        for (data, place) in zip(time_data, places):
+            if place is not '':
+                place = LectureTime.PLACE_TYPE.get(place.strip()[0][0], None)
+            else:
+                place = None
+            day = LectureTime.DAYS[data[0].strip()]
+            LectureTime.objects.create(lecture=lecture, start=data[1][0], end=data[1][1], day=day, place=place)
+
+        time_data.clear()
 
         if use_log is True:
             print(title + '(' + str(lecture.id) + ' | ' + code + ')')
